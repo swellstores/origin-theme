d81c9c4b52932377bfbe712c2712a49046482f15
Eric Ingram <eric@swell.is>
2020-07-28 06:37:21 -0600

    chore: remove unnecessary div wrapper

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <transition-group name="page-section" tag="div">
-      <div v-for="(section, index) in sections" :key="section.id">
-        <SectionAsyncLoader
-          :section="section"
-          :collection-index="index"
-          :fetch-is-pending="$fetchState.pending"
-        />
-      </div>
+      <SectionAsyncLoader
+        v-for="(section, index) in sections"
+        :key="section.id"
+        :section="section"
+        :collection-index="index"
+        :fetch-is-pending="$fetchState.pending"
+      />
     </transition-group>
   </div>
 </template>
