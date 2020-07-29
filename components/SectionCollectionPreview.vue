195be87c7cb27987b5d087ed390e19d95e673a68
Dave Loneragan <dave@papertokyo.com>
2020-07-29 17:31:12 +0200

    feat (collection preview): loading states

--- components/SectionCollectionPreview.vue
+++ components/SectionCollectionPreview.vue
@@ -1,6 +1,11 @@
 <template>
-  <!-- Collection preview -->
-  <section class="py-16">
+  <!-- Error/empty state -->
+  <div v-if="$fetchState.error" class="py-24 text-center">
+    <h3>SectionCollectionPreview</h3>
+    <p>{{ $fetchState.error.message }}</p>
+  </div>
+
+  <section v-else class="py-16">
     <div class="container">
       <div v-if="$fetchState.pending" class="loader-el w-64 h-8 mb-2 md:h-10"></div>
       <NuxtLink v-else :to="resolveUrl({ type: 'category', value: slug })">
@@ -18,15 +23,11 @@ export default {
   name: 'SectionCollectionPreview',
 
   props: {
+    type: {
+      type: String,
+      default: null
+    },
     id: {
-      type: String,
-      default: null
-    },
-    description: {
-      type: String,
-      default: null
-    },
-    name: {
       type: String,
       default: null
     },
@@ -37,29 +38,31 @@ export default {
   },
 
   async fetch() {
+    if (!this.categoryId) throw new Error(`No category selected`)
+
     const { $swell } = this
 
     // Set preload data
-    this.products = [...Array(6).keys()].map(() => ({}))
+    this.products = [...Array(3).keys()].map(() => ({}))
 
     // Fetch category with products
-    if (this.categoryId) {
-      const category = await $swell.categories.get(this.categoryId, { expand: 'products' })
+    const category = await $swell.categories.get(this.categoryId, { expand: 'products' })
 
-      // Set component data
-      if (category) {
-        this.slug = category.slug
-        this.products = category.products ? category.products.results : []
-      } else {
-        this.products = []
-      }
+    if (!category) {
+      throw new Error(`Category named "${this.name}" not found`)
     }
+
+    // Set component data
+    this.name = category.name
+    this.slug = category.slug
+    this.products = category.products ? category.products.results : []
   },
 
   data() {
     return {
-      products: [],
-      slug: ''
+      name: null,
+      slug: null,
+      products: []
     }
   },
 
