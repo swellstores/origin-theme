239659c99ab6bc9acc8903f11a720217c21ee84c
Dave Loneragan <dave@papertokyo.com>
2020-08-14 14:26:19 +0200

    chore: upgrade Tailwind module

--- tailwind.config.js
+++ tailwind.config.js
@@ -167,16 +167,5 @@ module.exports = {
 
       addUtilities(rotateUtilities, ['hover'])
     }
-  ],
-  purge: {
-    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      'components/**/*.vue',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'plugins/**/*.js',
-      'nuxt.config.js'
-    ]
-  }
+  ]
 }
