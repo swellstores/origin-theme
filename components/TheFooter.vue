d4759e02aa429f9c98f951bc85483ca2991087a3
Timothy Azarcon <timazarcon@Timothys-MacBook-Pro.local>
2020-10-23 21:02:45 +1100

    #944 -  Allow for footer BG theme.

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,6 +1,10 @@
 <template>
   <footer
-    class="bg-primary-darkest text-primary-lightest border-t border-primary-darker"
+    :class="{
+      'bg-primary-darkest text-primary-lightest border-t border-primary-darker':
+        background === 'dark',
+      'bg-primary-lighter border-t border-primary-med': background === 'light'
+    }"
     data-sw-path="footer"
   >
     <!-- Main footer -->
@@ -8,14 +12,14 @@
       <!-- Store info -->
       <div v-if="footer.showContactInfo || footer.showSocial" class="lg:w-1/4 lg:pr-6">
         <div v-if="footer.showContactInfo" class="mb-5">
-          <p v-if="footer.contactInfoHeading" class="text-sm text-primary-med mb-4">
+          <p v-if="footer.contactInfoHeading" class="text-sm mb-4">
             {{ footer.contactInfoHeading }}
           </p>
           <p>
-            <a class="block text-current" :href="'tel:' + store.supportPhone">
+            <a class="block" :href="'tel:' + store.supportPhone">
               {{ store.supportPhone }}
             </a>
-            <a class="block text-current" :href="'mailto:' + store.supportEmail">
+            <a class="block" :href="'mailto:' + store.supportEmail">
               {{ store.supportEmail }}
             </a>
           </p>
@@ -25,12 +29,7 @@
         <!-- Social links-->
         <ul v-if="footer.showSocial" class="mx-auto lg:-ml-2" data-sw-path="social">
           <li v-for="link of orderedSocialLinks" :key="link.id" class="inline-block mb-0">
-            <a
-              :href="link.url"
-              target="_blank"
-              :title="`Join us on ${link.id}`"
-              class="block mx-2 text-current"
-            >
+            <a :href="link.url" target="_blank" :title="`Join us on ${link.id}`" class="block mx-2">
               <BaseIcon :icon="`mdi:${link.id}`" />
             </a>
           </li>
@@ -45,7 +44,7 @@
           class="my-10 lg:w-1/4 lg:my-0"
         >
           <ul v-if="column.items">
-            <li v-if="showMenuLabel" class="text-sm text-primary-med mb-3">Explore</li>
+            <li v-if="showMenuLabel" class="text-sm mb-3">Explore</li>
             <li v-for="(item, i) in column.items" :key="item.name" class="mb-0">
               <!-- Heading -->
               <p
@@ -59,7 +58,7 @@
               <NuxtLink
                 v-else
                 :to="resolveUrl(item)"
-                class="block py-1 leading-tight text-xl text-primary-lightest"
+                class="inline-block py-1 leading-tight text-xl"
                 >{{ item.name }}</NuxtLink
               >
             </li>
@@ -71,21 +70,27 @@
       <template v-if="footer.showEmailSignup">
         <div class="max-w-96 mx-auto lg:w-1/4 lg:flex-shrink-0">
           <!-- Heading + text -->
-          <p v-if="footer.emailSignupHeading" class="text-sm text-primary-med mb-5">
+          <p v-if="footer.emailSignupHeading" class="text-sm mb-5">
             {{ footer.emailSignupHeading }}
           </p>
           <div v-if="footer.emailSignupText" v-html="footer.emailSignupText"></div>
-          <EmailSignupForm theme="dark" class="mt-5" />
+          <EmailSignupForm :theme="background === 'dark' ? 'dark' : 'light'" class="mt-5" />
         </div>
       </template>
     </div>
     <!-- END Main footer -->
 
     <!-- Secondary footer -->
-    <div class="py-4 border-t border-primary-darker text-sm text-center">
+    <div
+      :class="{
+        'border-primary-darker': background === 'dark',
+        'border-primary-med': background === 'light'
+      }"
+      class="py-4 border-t border-primary-darker text-sm text-center"
+    >
       <div class="container lg:flex lg:flex-no-wrap lg:justify-between">
         <!-- Copyright & Settings -->
-        <p class="my-3 text-sm text-primary-med">
+        <p class="my-3 text-sm">
           <span>©{{ currentYear }} {{ store.name }}.</span> Powered by
           <a
             id="powered-by-swell"
@@ -99,9 +104,7 @@
         <!-- Secondary nav menu -->
         <ul v-if="footer.showSecondaryMenu && secondaryMenu" class="my-3">
           <li v-for="item in secondaryMenu.items" :key="item.name" class="inline-block mx-3 mb-0">
-            <NuxtLink :to="resolveUrl(item)" class="text-primary-lightest">{{
-              item.name
-            }}</NuxtLink>
+            <NuxtLink :to="resolveUrl(item)">{{ item.name }}</NuxtLink>
           </li>
         </ul>
 
@@ -146,6 +149,7 @@ export default {
     this.store = $swell.settings.get('store', {})
     this.footer = $swell.settings.get('footer', {})
     this.socialLinks = $swell.settings.get('socialLinks', {})
+    this.background = $swell.settings.get('footer.background', 'dark')
   },
 
   data() {
@@ -156,7 +160,8 @@ export default {
       menu: null,
       secondaryMenu: null,
       paymentMethods: null,
-      currentYear: new Date().getFullYear()
+      currentYear: new Date().getFullYear(),
+      background: 'dark'
     }
   },
 
