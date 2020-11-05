ff583a525a148b7c175bf02a1f7f28bade5b5dd5
Timothy Azarcon <timazarcon@Timothys-MacBook-Pro.local>
2020-11-05 22:28:34 +1100

    Removed isAbsoluteURL function.

--- modules/swell/mixin.js
+++ modules/swell/mixin.js
@@ -9,8 +9,7 @@ Vue.use({
     Vue.mixin({
       methods: {
         formatMoney,
-        resolveUrl,
-        isAbsoluteURL
+        resolveUrl
       }
     })
 
@@ -79,11 +78,6 @@ function resolveUrl(item) {
 
     return itemPath // TODO wrap nuxt-i18n to generate localized path
   }
-}
-
-function isAbsoluteURL(url) {
-  const absoluteURLPattern = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi
-  return absoluteURLPattern.test(url)
 }
 
 function getContentPath({ type, value, url }) {
